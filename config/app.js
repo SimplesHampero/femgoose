f68d900a7cda24edd2ac603faf11678331b5cc31
Lee Brindley <leebrindley@gmail.com>
2017-04-25 13:34:32 +0100

    Added environment variable logic - the app accepts either 'dev' or 'prod' at the minute. Prod intialisation uses the cluster repo to spin up a new worker per available CPU.

---
+++ config/app.js
@@ -0,0 +1,11 @@
+module.exports = {
+	
+	//Default port to run the express application on
+	"port": 5555,
+
+	//Accepted environments variables to be passed to the app
+	"environments_available": [
+		"dev",
+		"prod"
+	]	
+};
---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const APP_CONFIG = require("./config/app");
+const bodyParser = require("body-parser");
+const StartUpManager = require("./lib/classes/start-up-manager");
+const numCPUs = require("os").cpus().length;
+const cluster = require("cluster");
+const compression = require("compression");
+
+//Perform intial initialization validation
+let initialised = StartUpManager.validateInit(); 
+
+if(initialised.result === false) {
+	console.log(init_ok.message);
+	return 0;
+}	
+
+//Define the express app object
+let app = express();
+
+//body-parser, used to parse request data
+app.use(bodyParser.json());
+
+//Compress request responses
+app.use(compression());
+
+if (initialised.current_environment === "dev") {
+
+	//Development setup
+
+	app.listen(APP_CONFIG.port, () => {
+		console.log("App listening on port " + APP_CONFIG.port);
+	});
+}
+else {
+	//Production setup
+
+	if (cluster.isMaster) {
+
+		// Fork workers.
+		for (let i = 0; i < numCPUs; i++) {
+			cluster.fork();
+		}
+
+		cluster.on('online', (worker, code, signal) => {
+			console.log(`worker ${worker.process.pid} online`);
+		})
+		.on('exit', (deadWorker, code, signal) => {
+
+			//If a worker dies restart it
+			var worker = cluster.fork();
+
+		    // Note the process IDs
+		    var newPID = worker.process.pid;
+		    var oldPID = deadWorker.process.pid;
+
+		    // Log the event
+		    console.log('worker '+oldPID+' died.');
+		    console.log('worker '+newPID+' born.');
+		});
+	} 
+	else {
+
+		// Start the server
+		app.listen(APP_CONFIG.port, () => {
+			console.log("App listening on port " + APP_CONFIG.port);
+		});
+	}
+}
