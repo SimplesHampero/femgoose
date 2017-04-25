81b27dfdaa7d9d815b8c99545418befb513189b2
Lee Brindley <leebrindley@gmail.com>
2017-04-25 15:06:14 +0100

    Added mongoose user model

---
+++ model/user.js
@@ -0,0 +1,13 @@
+const db = require("../db");
+
+let User = db.model("user", {
+	
+	username: { type: String, required: true, select: false },
+	password: { type: String, required: true, select: false },
+	
+	email: { type: String, required: false, select: false, default: "" },
+	first_name: { type: String, required: false, select: false, default: "" },
+	last_name: { type: String, required: false, select: false, default: "" },
+});
+
+module.exports = User;
