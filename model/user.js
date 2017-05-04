fafa63ed7683a6dc1721cad1a2903adffb9097b6
Lee Brindley <leebrindley@gmail.com>
2017-05-04 15:50:07 +0100

    Added validate password methods on the user model, both syncronous and asyncronous methods

--- model/user.js
+++ model/user.js
@@ -1,4 +1,5 @@
 const db = require("../db");
+const bcrypt = require("bcryptjs");
 
 let User = db.model("user", {
 	
@@ -9,5 +10,31 @@ let User = db.model("user", {
 	first_name: { type: String, required: false, select: false, default: "" },
 	last_name: { type: String, required: false, select: false, default: "" },
 });
+
+
+/*
+	Validate a user's password
+
+	returns cb (error, result) => result = {true, false}
+*/
+User.validatePassword = (password, cb) => {
+	bcrypt.compare(password, this.password, (err, result) => {
+		
+		if (err) {
+			return cb(err);
+		}
+
+		return cb(null, result);
+	});
+};
+
+/*
+	Validate a user's password syncronously
+
+	returns Boolean
+*/
+User.validatePasswordSync = (password, cb) => {
+	return bcrypt.compareSync(password, this.password);
+};
 
 module.exports = User;
