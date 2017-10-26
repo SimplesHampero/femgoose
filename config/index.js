775d78d24d34d5471251ee1c04e4495b97a002ed
Lee Brindley <leebrindley@gmail.com>
2017-10-26 13:20:32 +0100

    Update index.js
    Made the comment on the export more clear as to what's going on, it is no longer 'hard-coded' to working with development/production.

--- config/index.js
+++ config/index.js
@@ -33,5 +33,5 @@
 
 const ENV = process.env.NODE_ENV || "development";
 
-//Export the config respective of the environment the app is running on development/production
+//Export the config respective of the environment the app is running on, e.g. development/staging/production.
 module.exports = require(`./config.${ENV}.js`);
